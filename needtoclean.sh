4c1fdfb6478dbc6dd10ffbefb110ce720a424fe2
rhesus123 <rhesus@freemail.hu>
2016-09-14 12:19:26 +0200

    basic cleaning part 1

--- needtoclean.sh
+++ needtoclean.sh
@@ -1,28 +1,28 @@
 date >>$logfile
 
 
-#for k in /data/szolo/reads/21_1_*.txt.gz ; do
-#        echo $k  "start"   >>$logfile
-#        date >> $logfile
-#        filename=`basename $k .txt.gz`
-#       second="${filename/_1_/_2_}"
-#       echo  $filename
-#       echo $second
+for k in /data/szolo/reads/21_1_*.txt.gz ; do
+        echo $k  "start"   >>$logfile
+        date >> $logfile
+        filename=`basename $k .txt.gz`
+       second="${filename/_1_/_2_}"
+       echo  $filename
+       echo $second
 
-#x      this will make the second fastq.gz come after the firts one in the bwa mem command
-#        bwa mem -t 4   "${ensembl}" $k /data/szolo/reads/"${second}".txt.gz   2>"${logdir}"/"${filename}".memerr | 
-#        samtools view -Sb -t $datafai - 2>"${logdir}"/"${filename}"2.viewerr >"${basedir}"bam/"${filename}"unsorted.bam
-#       samtools sort  "${basedir}"bam/"${filename}"unsorted.bam  > "${basedir}"bam/both"${filename}".bam   2>"${logdir}"/"${filename}".sorterr 
-#        samtools index "${basedir}"bam/both"${filename}".bam  "${basedir}"bam/both"${filename}".bai
-#        echo "single $k ready"
-#       rm "${filename}"unsorted.bam
-#done
+#      this will make the second fastq.gz come after the firts one in the bwa mem command
+        bwa mem -t 4   "${ensembl}" $k /data/szolo/reads/"${second}".txt.gz   2>"${logdir}"/"${filename}".memerr | 
+        samtools view -Sb -t $datafai - 2>"${logdir}"/"${filename}"2.viewerr >"${basedir}"bam/"${filename}"unsorted.bam
+       samtools sort  "${basedir}"bam/"${filename}"unsorted.bam  > "${basedir}"bam/both"${filename}".bam   2>"${logdir}"/"${filename}".sorterr 
+        samtools index "${basedir}"bam/both"${filename}".bam  "${basedir}"bam/both"${filename}".bai
+        echo "single $k ready"
+       rm "${filename}"unsorted.bam
+done
 #singles done
 #so far we have converted the sra-s to bam-s and bai-s
 
 
 #-----------------
-#ha ez nem jo�, akkor a /data/szolo/genome/scriptz/srahasonlitas/sraprocessing/ sraprocess2.sh #ben van a kód egy másik, elvileukododo verzioja
+#if its not working, try sraprocess2.sh script 
 #----------------
 
 for p in ${basedir}bam/both*15*.bam
@@ -31,23 +31,23 @@ do
 
         filename=`basename $p .bam`
 
-#        samtools mpileup -uf $ensembl $p | bcftools call -mv -Oz  >${basedir}vcf/newest${filename}.vcf 2>${logdir}/${filename}.err
-#        echo "vcfstatting $k ready" >>$logfile
+        samtools mpileup -uf $ensembl $p | bcftools call -mv -Oz  >${basedir}vcf/newest${filename}.vcf 2>${logdir}/${filename}.err
+        echo "vcfstatting $k ready" >>$logfile
 
 #-----------------------------------
-#uj verzio, hatha ez lefut rendesen...
+#new version, hopefully it will run...
 #----------------------------------
 
-#samtools mpileup -uvf $ensembl $p >${basedir}vcf/uvfes${filename}.vcf
+samtools mpileup -uvf $ensembl $p >${basedir}vcf/uvfes${filename}.vcf
 
-#sweet, sweet tit licking miracle working baby of mary and josept, these files are big if you dont gzip them.
+#sweet, sweet tit licking baby of Mary and Joseph, these files are big if you dont gzip them.
 
-samtools mpileup -uvf $ensembl $p | gzip - >${basedir}vcf/uvfes${filename}.vcf.gz
+samtools mpileup -uvf $ensembl $p |gzip --stdout >${basedir}vcf/uvfes${filename}.vcf.gz
 
 done
 
 
 
-#date>>$logfile
+date>>$logfile
 
 exit
